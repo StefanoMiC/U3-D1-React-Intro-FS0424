7e5eafe1a9e7f3b80337859a5c04efa3b7ddb30f
Stefano Miceli <mic.ninja@gmail.com>
2024-06-24 13:12:20 +0200

    adds bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -28,13 +28,82 @@ import ClassComponent from "./components/ClassComponent";
 const App = () => {
   return (
     <div className="App">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
       {/* la Prop si crea semplicemente come se fosse un attributo di un tag, in questo caso l'attributo di un Componente è di fatto la sua Prop */}
       <Header
         logo={logo}
         id="signup"
         labelTxt="Iscriviti qui"
         placeholder="email@gmail.com"
-        className="App-header"
+        className="App-header shadow"
         // style in questo caso non è già applicato al componente, è semplicemente un valore ad oggetto che stiamo inviando come
         // prop che troveremo internamente al componente come props.style
         style={{ backgroundColor: "#D26100", border: "2px solid black" }}
