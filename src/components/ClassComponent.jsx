2cdc60e0661675b98033a376f20bb65ed0efc8ef
Stefano Miceli <mic.ninja@gmail.com>
2024-06-24 13:14:46 +0200

    adds one more comment

--- src/components/ClassComponent.jsx
+++ src/components/ClassComponent.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 class ClassComponent extends React.Component {
-  // render è un metodo OBBLIGATORIO quando usiamo un componente a Classe!
-  // è grazie al render che possiamo avere il return di un elemento JSX
+  // render è un metodo OBBLIGATORIO quando usiamo un componente a Classe! (non basta solo il return!)
+  // è grazie al render che possiamo avere il return di un elemento JSX al suo interno
+
+  // il suo scopo è quello di venire chiamato in un momento preciso nel ciclo di vita del componente (dopo che il componente si è montato)
+  // DEVE ritornare obbligatoriamente del JSX con le stesse caratteristiche degli altri componenti
   render() {
     console.log("THIS", this);
     console.log("PROPS", this.props);
